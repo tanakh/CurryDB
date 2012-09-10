6ca91953ec52b58a535826ba03af1d0f2590271b
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-09-10 16:35:23 +0900

    add test

--- test.hs
+++ test.hs
@@ -1,6 +1,9 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections     #-}
 
 import           Blaze.ByteString.Builder
+import           Control.Concurrent.Async
+import           Control.Monad
 import           Control.Monad.Trans
 import           Data.Conduit
 import           Data.Conduit.Attoparsec
@@ -12,7 +15,7 @@ import           Database.KVS             as KVS
 import           Database.Memcached
 
 main :: IO ()
-main = server
+main = server `race_` client
 
 server :: IO ()
 server = withSocketsDo $ runDBMT $ do
@@ -26,3 +29,18 @@ app src sink =
       liftIO $ putStrLn $ "server: " ++ show req
       resp <- lift $ execCommand req
       yield . toByteString . fromResponse $ resp
+
+client :: IO ()
+client = withSocketsDo $ do
+  runTCPClient (ClientSettings 3333 "localhost") $ \src sink ->
+    src $$ cond =$ sink
+  where
+    cond = do
+      resp1 <- yield "set foo 0 0 3\r\nbar\r\n" >> await
+      liftIO $ putStrLn $ "client : " ++ show resp1
+
+      resp2 <- yield "add foo 0 0 3\r\nbaz\r\n" >> await
+      liftIO $ putStrLn $ "client : " ++ show resp2
+
+      resp3 <- yield "get foo hoge\r\n" >> await
+      liftIO $ putStrLn $ "client : " ++ show resp3
