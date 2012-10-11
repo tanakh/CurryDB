371ee00a900b7fcd58e46aeb846069ffc6a75be2
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2012-10-11 22:15:06 +0900

    add command line option

--- redis.hs
+++ redis.hs
@@ -1,20 +1,44 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards   #-}
 
-import Database.Curry
-import Database.Redis
-import System.Remote.Monitoring (forkServer)
+import           Database.Curry
+import           Database.Redis
+import           Options.Applicative
+-- import           System.Remote.Monitoring (forkServer)
+import           Filesystem.Path.CurrentOS (decodeString)
 
-config :: Config
-config = def
-  { configPath = Just "dump.cdb"
-  , configSaveStrategy =
-    [ SaveByFrequency 900 1
-    , SaveByFrequency 300 10
-    , SaveByFrequency 60  10000
-    ]
+data Option = Option
+  { optPort :: Int
+  , optPath :: Maybe FilePath
   }
 
+opts :: Parser Option
+opts = Option
+  <$> option
+    ( long "port" & short 'p' & metavar "PORT"
+    & value 8854
+    & help "Port Number"
+    )
+  <*> option
+    ( long "file" & short 'f' & metavar "FILE"
+    & reader (Just . Just)
+    & value Nothing
+    & help "dump file"
+    )
+
 main :: IO ()
-main = do
-  forkServer "localhost" 8000
-  runServer config $ serverSettings 8854 "*"
+main = execParser optsInfo >>= \Option{..} -> do
+  -- forkServer "localhost" 8000 -- monitoring
+  (`runServer` serverSettings optPort "*") $ def
+    { configPath = fmap decodeString optPath
+    , configSaveStrategy =
+      [ SaveByFrequency 900 1
+      , SaveByFrequency 300 10
+      , SaveByFrequency 60  10000
+      ]
+    }
+  where
+    optsInfo = info (helper <*> opts)
+      ( fullDesc
+      & header "curry-redis - a redis clone over CurryDB"
+      )
